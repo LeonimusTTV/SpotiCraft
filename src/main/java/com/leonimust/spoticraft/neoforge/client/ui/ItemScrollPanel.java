eaded897761d6006a76292b56994123cf78a9abe
LeonimusT <leonimustytb@gmail.com>
2025-03-21 10:28:29 +0100

    feat: added shuffle and repeat buttons fix: liked button position fix: refresh token not being saved when access token is refreshed

--- src/main/java/com/leonimust/spoticraft/neoforge/client/ui/ItemScrollPanel.java
+++ src/main/java/com/leonimust/spoticraft/neoforge/client/ui/ItemScrollPanel.java
@@ -1,6 +1,5 @@
 package com.leonimust.spoticraft.neoforge.client.ui;
 
-import com.leonimust.spoticraft.neoforge.client.ui.Item;
 import com.mojang.blaze3d.vertex.Tesselator;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
